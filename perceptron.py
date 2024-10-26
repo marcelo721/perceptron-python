ff72ecfe21e6720f175f9c8904bde5f02f70c567
Marceo Henrique de Sousa <marceloh.sousa@alu.ufc.br>
2024-10-25 21:49:39 -0300

    finished

--- perceptron.py
+++ perceptron.py
@@ -1,7 +1,7 @@
 import numpy as np
 
-entradas = np.array([1, 7, 5])
-pesos = np.array([0.8, 0.1, 0])
+inputs = np.array([1, 7, 5])
+outputs = np.array([0.8, 0.1, 0])
 
 def sum(e, p):
     return e.dot(p) #dot product
@@ -12,7 +12,7 @@ def stepFunction(sum):
     return 0
 
 
-s = sum(entradas, pesos)
+s = sum(inputs, outputs)
 print(s)
 
 result = stepFunction(s)
